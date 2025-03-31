8786711e15a91f6a1f02aded6fa483bccebbfb5b
meshach5667 <meshachzakumi@gmail.com>
2025-03-31 20:22:14 +0100

    Revert "fixed"
    This reverts commit e5673ae331cb6a2440cd8ac2e57cef66dbc3c70a.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Chatbot from "./chatbot";
+import YoutubeVideos from "./YoutubeVideos";
+
+function App() {
+    return (
+        <div>
+            <Chatbot />
+            <YoutubeVideos />
+        </div>
+    );
+}
+
+export default App;
